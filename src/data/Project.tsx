b498386ea2376caac2a6be37fd5acfab35ced4ad
alifannaufallyatha <athaalifan@gmail.com>
2025-06-02 21:36:49 +0700

    modify data image

--- src/data/Project.tsx
+++ src/data/Project.tsx
@@ -1,3 +1,5 @@
+
+
 export interface Project {
   id: number;
   title: string;
@@ -14,7 +16,7 @@ export const projects: Project[] = [
     title: "Back-End Peminjaman Ruangan DPMPTSP ACEH",
     description: "Make a sistem for room loans at the DPMPTSP ACEH office, using the Laravel framework with a focus on developing system logic.",
     category: "Web Development",
-    image: "/public/assets/image/dpmptsp.png",
+    image: "/assets/image/dpmptsp.png",
     technologies: ["LARAVEL", "MYSQL", "Tailwind", "PHP"],
     link: "https://bidangdatin.xyz/app/sipro/public/"
   },
@@ -23,7 +25,7 @@ export const projects: Project[] = [
     title: "Fit-buddy Website",
     description: "A modern dashboard for fitness enthusiasts for your ideal body dreams with an exercise program designed especially for you. and responsive design.",
     category: "Web Development",
-    image: "/public/assets/image/fit-buddy.png",
+    image: "/assets/image/fit-buddy.png",
     technologies: ["React js + Vite", "Tailwind", "React hooks"],
     link: "https://fit-buddy-git-landing-page-alifannaufallyathas-projects.vercel.app/"
   },
@@ -32,7 +34,7 @@ export const projects: Project[] = [
     title: "APURA API Development",
     description: "RESTful API service with authentication, database integration, and documentation.",
     category: "Backend",
-    image: "/public/assets/image/apura.png",
+    image: "/assets/image/apura.png",
     technologies: ["Node.js","Typescript", "Express", "PostgreSQL", "JWT", "Postman", "Cloudinary", "Nodemailer"],
     link: "https://documenter.getpostman.com/view/31404023/2sB2qgddg4"
   },
@@ -41,7 +43,7 @@ export const projects: Project[] = [
     title: "MOOD MEAL API Development",
     description: "RESTful API service with authentication, database integration, and documentation.",
     category: "Backend",
-    image: "/public/assets/image/mood-meal-api-doc.png",
+    image: "/assets/image/mood-meal-api-doc.png",
     technologies: ["Node.js","Typescript", "Express", "PostgreSQL", "JWT", "Postman"],
     link: "https://documenter.getpostman.com/view/31404023/2sB2qgcxi4"
   },
@@ -50,7 +52,7 @@ export const projects: Project[] = [
     title: "Web Design DPMPTSP ACEH",
     description: "A modern and responsive web design for the DPMPTSP ACEH office, showcasing a clean and professional layout with a focus on user experience.",
     category: "Web Design",
-    image: "/public/assets/image/webdesign-dpmptsp.png",
+    image: "/assets/image/webdesign-dpmptsp.png",
     technologies: ["Figma"],
     link: "https://www.figma.com/proto/F8fKR12R0z5a7xv3MnAlBv/EBOOK-DPMPTSP?page-id=0%3A1&node-id=261-8922&viewport=2523%2C-3047%2C0.09&t=KHmmY7EJp4i6FI9N-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=8%3A142&show-proto-sidebar=1"
   },
@@ -59,7 +61,7 @@ export const projects: Project[] = [
     title: "Portfolio Website",
     description: "A modern and responsive portfolio website showcasing projects and skills with a clean design and then dark/light mode.",
     category: "Web Design",
-    image: "/public/assets/image/balohanweb.png",
+    image: "/assets/image/balohanweb.png",
     technologies: ["React + Vite", "TypeScript", "PostCSS","Tailwind", "Framer Motion", "Node.js"],
     link: "https://www.figma.com/proto/R4Kh8ZKVUXFS5opOKNCiKu/Web-Pelabohan?page-id=0%3A1&node-id=141-188&viewport=-363%2C-515%2C0.2&t=xVh90UUVlTlenI7t-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=23%3A2m"
   }
